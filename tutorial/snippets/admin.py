a641830188a09d5b8ca64603f9361fd531642ec3
Tung51056 <tung51056@donga.edu.vn>
2023-12-04 14:48:20 +0700

    Tutorial 4: Authentication & Permissions and Tutorial 5: Relationships & Hyperlinked APIs

--- tutorial/snippets/admin.py
+++ tutorial/snippets/admin.py
@@ -1,11 +1,11 @@
-# myapp/admin.py
+# snippets/admin.py
 from django.contrib import admin
 from .models import Snippet
 
 class SnippetAdmin(admin.ModelAdmin):
-    list_display = ('title', 'language', 'style', 'created')
-    list_filter = ('language', 'style', 'created')
-    search_fields = ('title', 'code')
+    list_display = ('title', 'owner', 'created')
+    list_filter = ('language', 'style', 'owner', 'created')
+    search_fields = ('title', 'code', 'owner__username')
     date_hierarchy = 'created'
     ordering = ('created',)
 
